50c7fe30f8c75a4fea09b518de964c24bab413cf
felipeoliveiraa98 <felipeoliveira.ffc@gmail.com>
2024-11-18 21:52:27 -0300

    estrutura do site

---
+++ src/components/Sobre/Sobre.jsx
@@ -0,0 +1,20 @@
+import loja from "../../assets/images/loja.png";
+import { Section, Div, Button } from "./style";
+
+export default function Sobre(){
+    return (
+      <Section>
+        <img src={loja} alt="" />
+        <Div>
+          <h3>PREPARAÇÃO</h3>
+          <h2>Níveis de torra</h2>
+          <p>
+            Qual a torra que prefere? Starbucks® Torra Clara, Torra Média ou
+            Torra Escura? Estas sãos as torras que fazem parte dos níveis de
+            torra Starbucks®
+          </p>
+          <Button>SAIBA MAIS</Button>
+        </Div>
+      </Section>
+    );
+}
